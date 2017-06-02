8dac0006cff20c102b0e7888f03d2a52db231a72
Evgenii Grigorev <grigorev.evgenii@gmail.com>
2017-06-02 14:06:13 +0300

    Regression with tree (all data)

--- main.R
+++ main.R
@@ -37,6 +37,7 @@ reg <- lm(koeff_otsenki ~ razryad_sotrydnika_max_za_period + vozrast_let	+ kolic
           koeff_rabochego_vremeni +	prirost_greida_za_kvartal	+ prirost_oklada_za_kvartal	+ obshii_shtat_v_podchinenii	+
           staj_v_sberbanke + summarnoe_kol_vo_let_obych_v_vyzah	+ sredniy_razryad_sotrydnika_za_period
           , data=rdata)
+
 summary(reg) # show results
 
 
@@ -53,18 +54,18 @@ plot(reg)
 
 #График зависимости "переменных / факторов"
 # pairs(rdata[11:20],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
-pairs(rdata[6:10],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
-pairs(rdata[10:14],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
-pairs(rdata[14:17],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
+# pairs(rdata[16:19],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
+# pairs(rdata[20:23],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
+# pairs(rdata[14:17],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
 
 
-coefficients(reg) # model coefficients
-confint(reg, level=0.95) # CIs for model parameters 
-fitted(reg) # predicted values
-residuals(reg) # residuals
-anova(reg) # anova table 
-vcov(reg) # covariance matrix for model parameters 
-influence(reg) # regression diagnostics
+# coefficients(reg) # model coefficients
+# confint(reg, level=0.95) # CIs for model parameters 
+# fitted(reg) # predicted values
+# residuals(reg) # residuals
+# anova(reg) # anova table 
+# vcov(reg) # covariance matrix for model parameters 
+# influence(reg) # regression diagnostics
 
 
 # Split randomly
@@ -76,10 +77,33 @@ data.target.split.evaluate <- data.target.split[(floor(nrow(data.target.split)*.
 library(rpart)
 
 # Rpart
-data.target.split.rpart <- rpart(data.target.split.train$success ~ max_razryad_za_period + vozrast + kolvo_obrazovanii
-                                 + kolvo_detei + let_posle_vyza + koeff_otrab_vremeni + pol + grade + status_na_mom_voznagrazdeniya
-                                 + ychen_stepen 
+data.target.split.rpart <- rpart(data.target.split.train$success ~ razryad_sotrydnika_max_za_period + vozrast_let	+ kolichestvo_vyshih_obrazovanii	+ 
+                                   kolichestvo_detei +	kol_vo_dnei_na_bolnichnom_za_kvartal	+ kolichestvo_dnei_neoplachiemogo_otpyska_za_kv	+
+                                   kol_vo_dnei_opl_otpyska_za_kvartal + kol_vo_let_posle_poslednego_vyshego_obr	+ koeff_otrabot_vremeni	+
+                                   koeff_rabochego_vremeni +	prirost_greida_za_kvartal	+ prirost_oklada_za_kvartal	+ obshii_shtat_v_podchinenii	+
+                                   staj_v_sberbanke + summarnoe_kol_vo_let_obych_v_vyzah	+ sredniy_razryad_sotrydnika_za_period
+                                 
                                  , data = data.target.split.train, method = "class", cp = 0.011, na.action = na.rpart)
+
+
+data.target.split.rpart <- rpart(data.target.split.train$success ~ top_300_16_plus + status_na_moment_voznagrazdeniya	+ 
+                                   semeinyi_status_text +	kod_semeinogo_statusa +	flag_prepodavanie_sb	+	
+                                   flag_naliciya_uchenoi_step	+ maimenovanie_uchenoi_stepeni	+ pol
+                                   
+                                 , data = data.target.split.train, method = "class", cp = 0.011, na.action = na.rpart)
+
+
+# data.target.split.rpart <- rpart(data.target.split.train$success ~ top_300_16_plus + status_na_moment_voznagrazdeniya	+ 
+#                                  semeinyi_status_text +	kod_semeinogo_statusa +	flag_prepodavanie_sb	+ date_birth +	
+#                                  flag_naliciya_uchenoi_step	+ maimenovanie_uchenoi_stepeni	+ pol + 
+#                                  razryad_sotrydnika_max_za_period + vozrast_let	+ kolichestvo_vyshih_obrazovanii	+ 
+#                                  kolichestvo_detei +	kol_vo_dnei_na_bolnichnom_za_kvartal	+ kolichestvo_dnei_neoplachiemogo_otpyska_za_kv	+
+#                                  kol_vo_dnei_opl_otpyska_za_kvartal + kol_vo_let_posle_poslednego_vyshego_obr	+ koeff_otrabot_vremeni	+
+#                                  koeff_rabochego_vremeni +	prirost_greida_za_kvartal	+ prirost_oklada_za_kvartal	+ obshii_shtat_v_podchinenii	+
+#                                  staj_v_sberbanke + summarnoe_kol_vo_let_obych_v_vyzah	+ sredniy_razryad_sotrydnika_za_period
+#                                  
+#                                  , data = data.target.split.train, method = "class", cp = 0.011, na.action = na.rpart)
+
 
 summary(data.target.split.rpart)
 
@@ -102,14 +126,14 @@ data.target.split.evaluate$correct <- data.target.split.evaluate$prediction == d
 print(paste("% of predicted classifications correct", 100*mean(data.target.split.evaluate$correct)))
 table(data.target.split.evaluate$prediction, data.target.split.evaluate$success)
 
-# Проверка кластеров по дереву решений
-
-library(sqldf)
-
-# Calculate the overall accuracy.
-data.target.split.evaluate$correct <- data.target.split.evaluate$prediction == data.target.split.evaluate$success
-print(paste("% of predicted classifications correct", 100*mean(data.target.split.evaluate$correct)))
-table(data.target.split.evaluate$prediction, data.target.split.evaluate$success)
+# # Проверка кластеров по дереву решений
+# 
+# library(sqldf)
+# 
+# # Calculate the overall accuracy.
+# data.target.split.evaluate$correct <- data.target.split.evaluate$prediction == data.target.split.evaluate$success
+# print(paste("% of predicted classifications correct", 100*mean(data.target.split.evaluate$correct)))
+# table(data.target.split.evaluate$prediction, data.target.split.evaluate$success)
 
 
 
