1cbfe6cbd9e32359843e03ce179fa08d8ae99e88
Evgenii Grigorev <grigorev.evgenii@gmail.com>
2017-06-02 13:53:55 +0300

    Regression new data

--- main_84.R
+++ main_84.R
@@ -8,55 +8,45 @@ setwd(datDir)
 # Read data
 setwd(paste(datDir,"/pa",sep=""))
 
-base <- read.csv('base-02-06-2017.txt',header=TRUE, sep='\t', quote = "\'")
+base <- read.csv('base-01-06-2017.txt',header=TRUE, sep='\t', quote = "\'")
 
 
-summary(base$koeff_otsenki)
-plot(base$koeff_otsenki)
+summary(base)
+
+
+# plot(base$koeff_otcenki)
 
 # Фильтруем по показателям, которые доступны
-rdata <- base[which(base$koeff_otsenki != 'NaN'),]
-summary(rdata$koeff_otsenki)
+rdata <- base[which(base$koeff_otcenki != 'NaN'),]
+summary(rdata$koeff_otcenki)
 
-med <- median(rdata$koeff_otsenki)
+med <- median(rdata$koeff_otcenki)
 
 rdata$success <- 0
 
 # Целевая функция - если значение Кцели больше медианы, тогда сотрудник успешен
 for(i in 1:nrow(rdata))
-  if(rdata$koeff_otsenki[i] > med) rdata$success[i] <- 1
+  if(rdata$koeff_otcenki[i] > med) rdata$success[i] <- 1
 
 table(rdata$success)
 pie(table(rdata$success))
 
 
 # Multiple Linear Regression Example 
-reg <- lm(koeff_otsenki ~ razryad_sotrydnika_max_za_period + vozrast_let	+ kolichestvo_vyshih_obrazovanii	+ 
-          kolichestvo_detei +	kol_vo_dnei_na_bolnichnom_za_kvartal	+ kolichestvo_dnei_neoplachiemogo_otpyska_za_kv	+
-          kol_vo_dnei_opl_otpyska_za_kvartal + kol_vo_let_posle_poslednego_vyshego_obr	+ koeff_otrabot_vremeni	+
-          koeff_rabochego_vremeni +	prirost_greida_za_kvartal	+ prirost_oklada_za_kvartal	+ obshii_shtat_v_podchinenii	+
-          staj_v_sberbanke + summarnoe_kol_vo_let_obych_v_vyzah	+ sredniy_razryad_sotrydnika_za_period
+reg <- lm(koeff_otcenki ~ max_razryad_za_period + vozrast + koeff_rascheta_premii +kolvo_obrazovanii
+          + kolvo_detei + let_posle_vyza + koeff_otrab_vremeni
           , data=rdata)
 summary(reg) # show results
-
 
 # diagnostic plots 
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page 
 plot(reg)
-
-# abline(lm(koeff_otsenki ~ max_razryad_za_period , data=rdata))
-
-# # K-fold cross-validation
-# library(DAAG)
-# cv.lm(df=rdata, reg, m=3) # 3 fold cross-validation
 
 
 #График зависимости "переменных / факторов"
 # pairs(rdata[11:20],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
 pairs(rdata[6:10],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
 pairs(rdata[10:14],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
-pairs(rdata[14:17],main="Main Data", pch=19, col=as.numeric(rdata$success)+1)
-
 
 coefficients(reg) # model coefficients
 confint(reg, level=0.95) # CIs for model parameters 
@@ -76,9 +66,8 @@ data.target.split.evaluate <- data.target.split[(floor(nrow(data.target.split)*.
 library(rpart)
 
 # Rpart
-data.target.split.rpart <- rpart(data.target.split.train$success ~ max_razryad_za_period + vozrast + kolvo_obrazovanii
-                                 + kolvo_detei + let_posle_vyza + koeff_otrab_vremeni + pol + grade + status_na_mom_voznagrazdeniya
-                                 + ychen_stepen 
+data.target.split.rpart <- rpart(data.target.split.train$success ~ max_razryad_za_period + vozrast + koeff_rascheta_premii +kolvo_obrazovanii
+                                 + kolvo_detei + let_posle_vyza + koeff_otrab_vremeni
                                  , data = data.target.split.train, method = "class", cp = 0.011, na.action = na.rpart)
 
 summary(data.target.split.rpart)
@@ -111,18 +100,6 @@ data.target.split.evaluate$correct <- data.target.split.evaluate$prediction == d
 print(paste("% of predicted classifications correct", 100*mean(data.target.split.evaluate$correct)))
 table(data.target.split.evaluate$prediction, data.target.split.evaluate$success)
 
-
-
-library(randomForest)
-data.target.split.rforest <- randomForest(data.target.split.train$success ~ 
-                                            max_razryad_za_period + vozrast + kolvo_obrazovanii
-                                          + kolvo_detei + let_posle_vyza + koeff_otrab_vremeni + pol + grade + status_na_mom_voznagrazdeniya
-                                          + ychen_stepen, 
-                                          data = data.target.split.train , na.action=na.omit)
-
-print(data.target.split.rforest) # view results 
-importance(data.target.split.rforest) # importance of each predictor
-plot(data.target.split.rforest)
 
 # Если в модели оставить только тех, кого предсказывает, как успешные "1", тогда точность модели составит ~80%
 # accurasy <- sqldf("SELECT count(tabelnyi), success,prediction from che t group by success,prediction")
